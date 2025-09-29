0290eaeff13ffba3106e37de855e111de2c2c154
Pablo Lires <pablolires@gmail.com>
2025-09-29 15:02:30 -0300

    Unificacion de header-backend en archivo aparte

---
+++ backend/includes/header-backend.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-5">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="navbar-brand" href="#">
+      <img src="../img/logo-unique-footer.png" alt="logo">
+    </a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item">
+        <a class="nav-link" href="posts.php">Posts</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" data-toggle="modal" data-target="#modalFormUser">Mi Usuario</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="php/logout.php">Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
--- backend/posts.php
+++ backend/posts.php
@@ -167,30 +167,8 @@ include_once('../includes/config.inc.php');
     <!-- Loader -->
     <?php require('includes/loader.inc.php'); ?>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-5">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <a class="navbar-brand" href="#">
-          <img src="../img/logo-unique-footer.png" alt="logo">
-        </a>
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="jobs.php">Trabajos</a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="posts.php">Posts</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" data-toggle="modal" data-target="#modalFormUser">Mi Usuario</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="php/logout.php">Logout</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <!-- Loader -->
+    <?php require('includes/header-backend.php'); ?>
 
     <!-- Mensajes de éxito -->
     <div style="z-index: 9999;" v-if="messages.length" id="messages" class="alert alert-success alert-dismissible fade show fadeInLeft" role="alert">
