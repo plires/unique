ac2f88a5ec57b1168c2dc135d6f50f6c2055614c
Pablo Lires <pablolires@gmail.com>
2025-09-29 14:32:59 -0300

    Refactor. backend/php/login.php

--- backend/php/login.php
+++ backend/php/login.php
@@ -2,18 +2,20 @@
 
 include('con.php');
 
-$sql = "SELECT * FROM users;";
+$sql = "SELECT * FROM users WHERE user = :user AND pass = :password;";
 $stmt = $db->prepare($sql);
-$stmt->execute();
-$userBdd = ($stmt->fetch(PDO::FETCH_ASSOC));
 
-$user = $_POST['user'];
-$pass = $_POST['password'];
+$stmt->execute([
+	':user' => $_POST['user'],
+	':password' => md5($_POST['password'])
+]);
 
-if ($userBdd['user'] === $user && $userBdd['pass'] === md5($pass)) {
+$userBdd = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($userBdd) {
 	session_start();
-	$_SESSION['id'] = (int)$user;
-	$_SESSION['user'] = $pass;
+	$_SESSION['id'] = (int)$userBdd['id'];
+	$_SESSION['user'] = $userBdd['user'];
 	header('Location: posts.php');
 } else {
 	$errors['match'] = 'usuario o Contraseña incorrecta.';
