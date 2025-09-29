39684869ff3b39b37367c67d0cb1769c14e43bc8
Pablo Lires <pablolires@gmail.com>
2025-09-29 15:41:40 -0300

    Fix en/blog.php. Cambio de src en script wow.min.js

--- en/blog.php
+++ en/blog.php
@@ -233,7 +233,7 @@ require_once("../clases/repositorioSQL.php");
   <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-  <script src="node_modules/wowjs/dist/wow.min.js"></script>
+  <script src="../node_modules/wowjs/dist/wow.min.js"></script>
   <script src="../js/app.js"></script>
   <script src="../js/blog.js"></script>
 
